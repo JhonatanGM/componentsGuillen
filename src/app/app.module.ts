10ed9d1e279af9584e5099833b8cf348d8999eb5
Jhonatan <Jhonatan_gc15@hotmail.com>
2022-12-20 17:25:06 -0500

    desafio1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './layout/menu/menu.component';
+import { ToolbarComponent } from './layout/toolbar/toolbar.component';
+import { ContenidoComponent } from './layout/contenido/contenido.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ToolbarComponent,
+    ContenidoComponent
   ],
   imports: [
     BrowserModule,
